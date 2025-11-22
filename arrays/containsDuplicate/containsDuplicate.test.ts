e9e6133fea7f017d84606df21668130779cde771
Miki Palao <miki.palao@mrmilu.com>
2025-11-22 17:25:45 +0100

    Implement containsDuplicate function and add comprehensive tests

--- arrays/containsDuplicate/containsDuplicate.test.ts
+++ arrays/containsDuplicate/containsDuplicate.test.ts
@@ -1,5 +1,43 @@
-describe('containsDuplicate - dummy test', () => {
-  it('placeholder: always passes', () => {
-    expect(true).toBe(true);
+import { containsDuplicate } from './containsDuplicate';
+
+describe('Contains Duplicate', () => {
+  it('if the array contains duplicated numbers it should return true', () => {
+    const arrayWithDuplicates = [1, 2, 3, 1];
+    expect(containsDuplicate(arrayWithDuplicates)).toBe(true);
+  });
+
+  it('if the array do not contains duplicated numbers it should return false', () => {
+    const arrayWithDuplicates = [1, 2, 3, 4];
+    expect(containsDuplicate(arrayWithDuplicates)).toBe(false);
+  });
+
+  it('should return true for an array with multiple duplicates', () => {
+    const arrayWithMultipleDuplicates = [1, 2, 3, 1, 2, 3];
+    expect(containsDuplicate(arrayWithMultipleDuplicates)).toBe(true);
+  });
+
+  it('should return false for an empty array', () => {
+    const emptyArray: number[] = [];
+    expect(containsDuplicate(emptyArray)).toBe(false);
+  });
+
+  it('should return false for an array with one element', () => {
+    const singleElementArray = [1];
+    expect(containsDuplicate(singleElementArray)).toBe(false);
+  });
+
+  it('should return true for an array with negative numbers', () => {
+    const arrayWithNegatives = [-1, -2, -3, -1];
+    expect(containsDuplicate(arrayWithNegatives)).toBe(true);
+  });
+
+  it('should return true for an array with mixed positive and negative duplicates', () => {
+    const mixedArray = [1, -1, 2, -1];
+    expect(containsDuplicate(mixedArray)).toBe(true);
+  });
+
+  it('should return false for an array with mixed the same number positive and negative ', () => {
+    const mixedArray = [1, -2, 2, -1];
+    expect(containsDuplicate(mixedArray)).toBe(false);
   });
 });
---
+++ arrays/containsDuplicate/containsDuplicate.ts
@@ -0,0 +1,12 @@
+export const containsDuplicate = (nums: Array<number>) => {
+  const set = new Set();
+
+  for (let i = 0; i < nums.length; i++) {
+    if (set.has(nums[i])) {
+      return true;
+    }
+    set.add(nums[i]);
+  }
+
+  return false;
+};
